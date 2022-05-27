3745d43f52de45eb3d788e565743d49bb20bc399
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-05-27 20:33:16 +0300

    tiny mce text editor

---
+++ index.js
@@ -0,0 +1,11 @@
+// --------------------------------- TEXT EDITOR
+tinymce.init({
+  selector: '.form-wysiwyg__editor',
+  menubar: false,
+  statusbar: false,
+  plugins: 'lists',
+  toolbar: 'bold italic underline bullist alignjustify alignleft alignright forecolor',
+  icons: 'material',
+  min_height: 150,
+  height: '100%',
+});

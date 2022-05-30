348ba006fb0e79a1e1b351293f4d988c0a1558d3
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-05-30 18:17:25 +0300

    inputSearch; section; banner

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,5 +1,35 @@
+function highlightDot(slider, i) {
+  const dots = slider.querySelector('.slider__controls').childNodes;
+  dots.forEach(element => element.className = 'sliderDot');
+  dots[i].className = 'sliderDot sliderDot--active'
+}
+function showSlide(allSlides, i) {
+  allSlides.forEach(element => element.style.display = 'none')
+  allSlides[i].style.display = 'flex';
+}
 
+function initSlider(slider) {
+  const allSlides = slider.querySelectorAll('.slide');
+  const sliderControls = slider.querySelector('.slider__controls');
 
-module.exports = header
+  for (let i = 0; i < allSlides.length; i++) {
+    const newDot = document.createElement('div');
+    newDot.className = 'sliderDot';
+    newDot.addEventListener('click', () => {
+      showSlide(allSlides, i);
+      highlightDot(slider, i);
+    })
+    sliderControls.append(newDot)
+  }
 
+  showSlide(allSlides, 0);
+  highlightDot(slider, 0)
+}
 
+const allSliders = document.querySelectorAll(`.slider`);
+
+function initAllSliders() {
+  allSliders.forEach(element => initSlider(element))
+}
+
+module.exports = initAllSliders
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./reset.css";
 import "./style.css";
-// const state = require("./render.js");
 
 import html from './tech/tech.html';
 const techHtml = document.createElement('html');
 techHtml.innerHTML = html;
 
+// ----------------------------------------- HEADER
 import logo from './assets/Marvel_Logo.png';
 import hamburger from './assets/Hamburger_icon_white.png';
 import cart from './assets/cart.svg';
@@ -14,16 +14,16 @@ import userpic from './assets/user0.png';
 const header = techHtml.querySelector('.header');
 document.body.append(header)
 
+// ----------------------------------------- SLIDER
+import bannerBg from './assets/banner-bg.jpg';
+import searchButton from './assets/search-button.svg'
+import slide1 from './assets/slide1.jpg'
+import slide2 from './assets/slide2.jpg'
+import slide3 from './assets/slide3.jpg'
+import slide4 from './assets/slide4.jpg'
 
+const banner = techHtml.querySelector('.banner');
+document.body.append(banner);
+const initSlider = require('./components/slider')
+initSlider()
 
-// --------------------------------- TEXT EDITOR
-// tinymce.init({
-//   selector: '.form-wysiwyg__editor',
-//   menubar: false,
-//   statusbar: false,
-//   plugins: 'lists',
-//   toolbar: 'bold italic underline bullist alignjustify alignleft alignright forecolor',
-//   icons: 'material',
-//   min_height: 150,
-//   height: '100%',
-// });
